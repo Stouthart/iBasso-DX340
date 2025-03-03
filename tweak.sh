8ceb389c9e10ca218859fce1ced1eb1c26083914
Stouthart <github@stouthart.nl>
2025-03-03 15:46:48 +0100

    Update tweak.sh

--- tweak.sh
+++ tweak.sh
@@ -2,8 +2,8 @@
 
 # Check preconditions
 err_msg() { printf '\e[31mError: %s\e[m\n' "$1"; }
-[ "$(whoami)" != root ] && err_msg 'root privileges required. Did you run adb root?' && exit 1
-! touch /etc/init/custom.rc 2>/dev/null && err_msg 'filesystem not rw. Did you run adb remount?' && exit 1
+[ "$(whoami)" != root ] && err_msg 'Root privileges required. Did you run adb root?' && exit 1
+! touch /etc/init/custom.rc 2>/dev/null && err_msg 'Read-only file system. Did you run adb remount?' && exit 1
 
 echo 'Running...'
 
