89a9193048a298a3bcbc2e739184dd0a7fcf2532
Stouthart <github@stouthart.nl>
2025-03-03 13:41:22 +0100

    Add files via upload

---
+++ tweak.sh
@@ -0,0 +1,79 @@
+#!/bin/sh
+
+# Check preconditions
+err_msg() { printf '\e[31mError: %s\e[m\n' "$1"; }
+[ "$(whoami)" != root ] && err_msg 'user not root' && exit 1
+! touch /etc/init/custom.rc 2>/dev/null && err_msg 'filesystem not rw' && exit 1
+
+
+## https://android.stackexchange.com/questions/217495/how-can-proc-sys-values-be-changed-at-boot-sysctl-conf-does-this-on-normal-lin
+## https://android.googlesource.com/platform/system/core/+/master/init/README.md
+
+cat >/etc/init/custom.rc <<EOF
+on init
+  write /sys/kernel/debug/debug_enabled N
+
+  ## https://github.com/redhat-performance/tuned/tree/master/profiles
+  write /proc/sys/kernel/timer_migration 0
+  write /proc/sys/net/core/somaxconn 2048
+  write /proc/sys/net/ipv4/tcp_fastopen 3
+  write /proc/sys/vm/dirty_background_ratio 3
+  write /proc/sys/vm/dirty_ratio 10
+  write /proc/sys/vm/stat_interval 10
+
+on late-init
+  # Disable scheduler statistics to reduce overhead (enabled in /etc/init/atrace.rc) 
+  write /proc/sys/kernel/sched_schedstats 0
+
+on late-fs
+  ## https://source.android.com/docs/core/perf/boot-times#tuning-the-filesystem
+  write /sys/block/sda/queue/iostats 0
+  write /sys/block/sda/queue/read_ahead_kb 2048
+
+on property:sys.boot_completed=1
+  # Runtime SchedTune
+  write /dev/stune/schedtune.boost 10
+  write /dev/stune/foreground/schedtune.boost 20
+  write /dev/stune/foreground/schedtune.prefer_idle 1
+  write /dev/stune/top-app/schedtune.boost 30
+  write /dev/stune/top-app/schedtune.prefer_idle 1
+
+  # Fork and execute /etc/rc.local
+  exec_background -- /etc/rc.local
+EOF
+
+chmod 0644 /etc/init/custom.rc
+chcon u:object_r:system_file:s0 /etc/init/custom.rc
+
+
+cat >/etc/rc.local <<EOF
+#!${SHELL}
+
+sleep 10
+
+# Runtime TuneD
+echo 10 >/proc/sys/vm/swappiness
+
+## https://xdaforums.com/t/how-to-set-permanent-background-process-limit.1869712#post-89958060
+service call activity 44 i32 4 >/dev/null
+
+## https://github.com/LeanxModulostk/IRQ-Balancer-Configuration/blob/main/service.sh
+renice -n -10 -p "\$(pidof msm_irqbalance)"
+
+# Disable IPv6
+echo 1 >/proc/sys/net/ipv6/conf/all/disable_ipv6
+
+# SD card tuning (if found)
+[ -f /sys/block/mmcblk0/queue/iostats ] && echo 0 >/sys/block/mmcblk0/queue/iostats
+
+exit 0
+
+EOF
+
+chmod +x /etc/rc.local
+
+
+echo 'Rebooting...'
+reboot
+
+exit 0
